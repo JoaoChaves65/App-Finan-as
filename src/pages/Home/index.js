70ffd456882da087778ccd617793fa481f622062
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-04-14 16:26:32 -0300

    Header do app

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-import  Header  from '../components/Header';
+import  Header  from '../../components/Header';
+import Balance from '../../components/Balance'
 
 export default function Home() {
   return (
