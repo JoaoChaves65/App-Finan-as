a06b021e4f41397048f2fe0ade4aee7bba9c29be
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-04-15 12:38:49 -0300

    Movimentações da conta, e sendo possível mostrar ou esconder os valores

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import  Header  from '../../components/Header';
 import Balance from '../../components/Balance'
+import Movements from '../../components/Movements'
+
+const list = [
+  {
+    id: 1,
+    label: "Boleto conta luz",
+    value: "300,90",
+    date: "17/09/2022",
+    type: 0 //despesas
+  },
+  {
+    id: 2,
+    label: "Pix cliente X",
+    value: "2.500,00",
+    date: "22/09/2022",
+    type: 1 //receita
+  },
+  {
+    id: 3,
+    label: "Salário",
+    value: "7.200,00",
+    date: "22/09/2022",
+    type: 1 //receita
+  },
+]
 
 export default function Home() {
   return (
@@ -9,6 +34,16 @@ export default function Home() {
         <Header name="Matheus Henrique"/>
 
         <Balance saldo="9.250,90" gastos="-527,00"/>
+
+        <Text style={styles.title}>Últimas movimentações</Text>
+
+        <FlatList
+          style={styles.list}
+          data={list}
+          keyExtractor={ (item) => String(item.id) }
+          showsVerticalScrollIndicator={false}
+          renderItem={ ({ item }) => <Movements data={item}/>}
+        />
 
     </View>
   );
@@ -19,4 +54,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FAFAFA',
   },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14, 
+    marginRight: 14,
+    marginTop: 14,
+  },
+  list:{
+    marginStart: 14,
+    marginEnd: 14,
+  }
 });
